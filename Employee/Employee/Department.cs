6d53aea3cb83cc4e384fcf1af03fdaa6b5a2e078
Ahmed <dev@test.com>
2021-06-03 16:21:48 +0300

    Done

---
+++ Employee/Employee/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Employee
+{
+    public class Department
+    {
+        public int id;
+        public String Name;
+
+        public Department(int id, string name)
+        {
+            this.id = id;
+            Name = name;
+        }
+
+        public String SetNameDepartment(String name)
+        {
+            return this.Name=name;
+        }
+
+        public String getNameDepartment()
+        {
+            return this.Name;
+        }
+        public Department()
+        {
+        }
+    }
+}
